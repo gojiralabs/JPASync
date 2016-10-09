c6206a29fa48e95b49fec101e43df8f89f3ace06
Robson Enke <robsonenke@gmail.com>
2016-10-09 00:54:53 -0300

    Started checker API

---
+++ src/main/java/com/gojiralabs/dwtca/common/Checker.java
@@ -0,0 +1,20 @@
+package com.gojiralabs.dwtca.common;
+
+import javax.annotation.Nonnull;
+
+public class Checker {
+
+	public static <T> T notNull(T object) {
+		if (object == null) {
+			throw new NullPointerException();
+		}
+		return object;
+	}
+
+	public static <T> T notNull(T object, @Nonnull String message, @Nonnull Object... messageParameters) {
+		if (object == null) {
+			throw new NullPointerException(String.format(message, messageParameters));
+		}
+		return object;
+	}
+}
