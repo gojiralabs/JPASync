e7fb53a2a7d2d22d76451ffc8ccf32e660009871
Robson Enke <robsonenke@gmail.com>
2016-10-10 13:50:42 -0300

    Adding more checks to Checker

--- src/main/java/com/gojiralabs/gojira/common/Checker.java
+++ src/main/java/com/gojiralabs/gojira/common/Checker.java
@@ -21,4 +21,58 @@ public class Checker {
 		}
 		return object;
 	}
+
+	public static void checkArrayIndex(@Nonnull boolean[] array, int index) {
+		checkArrayIndex(array.length, index);
+	}
+
+	public static void checkArrayIndex(@Nonnull byte[] array, int index) {
+		checkArrayIndex(array.length, index);
+	}
+
+	public static void checkArrayIndex(@Nonnull short[] array, int index) {
+		checkArrayIndex(array.length, index);
+	}
+
+	public static void checkArrayIndex(@Nonnull char[] array, int index) {
+		checkArrayIndex(array.length, index);
+	}
+
+	public static void checkArrayIndex(@Nonnull int[] array, int index) {
+		checkArrayIndex(array.length, index);
+	}
+
+	public static void checkArrayIndex(@Nonnull long[] array, int index) {
+		checkArrayIndex(array.length, index);
+	}
+
+	public static void checkArrayIndex(@Nonnull float[] array, int index) {
+		checkArrayIndex(array.length, index);
+	}
+
+	public static void checkArrayIndex(@Nonnull double[] array, int index) {
+		checkArrayIndex(array.length, index);
+	}
+
+	public static <T> void checkArrayIndex(@Nonnull T[] array, int index) {
+		checkArrayIndex(array.length, index);
+	}
+
+	private static void checkArrayIndex(int arrayLength, int index) {
+		if (index < 0 || index >= arrayLength) {
+			throw new ArrayIndexOutOfBoundsException(index);
+		}
+	}
+
+	public static void checkArgument(boolean condition) {
+		if (!condition) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public static void checkArgument(boolean condition, @Nonnull String message, Object... messageParameters) {
+		if (!condition) {
+			throw new IllegalArgumentException(String.format(message, messageParameters));
+		}
+	}
 }
